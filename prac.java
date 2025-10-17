30d0506a20c0c3c8adafa8eca635636ac7e0c310
0001monish <monishkp2006@gmail.com>
2025-10-17 19:19:13 +0530

    ok

--- prac.java
+++ prac.java
@@ -1,11 +1,24 @@
 import java.util.*;
-class prac{
-    public static void main(String args[]){
-        List<String> list =new ArrayList<>();
-        list.add("a");
-        list.add("b");
-        list.add("c");
-        System.out.print(list+"\n");
-        list.forEach(lis->System.out.print(lis));
-    }
-}
+// Node class
+ 
+// Linked List cl
+// Main function to test
+public class prac {
+    public static void main(String[] args) {
+         int num[]={1,2,5,0,4};
+         int i=0,j=1,k=0,second=0;
+         for(j=1;j<num.length;j++){
+            if(num[j]>num[i]){
+                second=j;
+                i=j;
+            }
+            else if(num[i]>second && num[i]!=i){
+                second=num[i];
+
+            }
+         }
+         System.out.println(second);
+         }
+
+         
+     }
